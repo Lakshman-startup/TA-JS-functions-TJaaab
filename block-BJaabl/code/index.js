bd0a5fafb4fd0fbca87c3e6994df975bc8795119
suresh <lakshman.team2020@gmail.com>
2021-02-21 23:28:35 -0800

    done

--- block-BJaabl/code/index.js
+++ block-BJaabl/code/index.js
@@ -2,7 +2,16 @@
 1. Create a function named `sayHello` that that accepts a parameter `name` and alert `Hello [name]!`.
 */
 
-/*
+function sayHello(name) {
+  alert(name)
+}
+
+/* 
+
+
+
+
+
 2. Create a function named `getFullName` that accepts two parameter `firstName` and `lastName` and returns
 fullName.
 
@@ -10,6 +19,13 @@ Example:
   getFullName("John", "Snow"); // "John Snow"
   getFullName("Nelson", "Mandela"); // "Nelson Mandela"
 */
+function getFullname(firstName, lastName) 
+
+{
+  let fullName= `${firstName} ${lastName}`
+  return fullName
+
+
 
 /*
 3. Create a function named `addTwoNumbers` that accepts two numbers i.e `firstNum` and `secondNum` and returns
@@ -20,6 +36,24 @@ addTwoNumbers(10, 22); // 32
 addTwoNumbers(20, 32); // 32
 addTwoNumbers(10, "100"); // Alert Enter Valid Input
 */
+function addTwoNumbers(firstNum, secondNum) 
+
+{
+  if (typeof firstNum=="number" && typeof secondNum=='number') 
+   
+  {
+    let sumNam= `${firstNum} ${lastNum}`
+
+    return sumNam
+
+  } else {
+    alert('enter valid input')
+    
+  }
+  
+
+
+
 
 /*
 4. Create a function named `calc` which accepts three parameter `numA`, `numB` and `operation`. Operation
@@ -33,6 +67,17 @@ calc(20, 10, 'sub'); // 10
 calc(20, 10, 'mul'); // 200
 */
 
+function calc(numA,numB,operation) 
+
+{
+  if (typeof numA=="number" && typeof numB=='number') 
+   
+    
+  {
+    alert('enter valid input')
+    
+  }
+
 
 /*
 5. Create a function named `isLeapYear` that accepts a number data type and return `true` or `false` based
@@ -42,7 +87,44 @@ isLeapYear(2000); // true
 isLeapYear(2001); // false
 */
 
+function isLeapYear(numA) {
+  if (typeof numA=="number") {
+    if (numA%4===0 ) {
+
+     return true
+    
+    }else{ 
+      
+      return false
+
+
+    } 
+  }   
+  else {
+
+    alert('invalidinput')
+  }
+}
+
+
+
+
+
 
 /*
 6. Create a function named `getFactorial` that accepts a number and return the factorial of the number.
 */
+function getFactorial(numA) 
+{
+  if (typeof numA=="number") {
+    if (numA>0 ) {
+ return numA*getFactorial(numA-1) 
+     
+    
+    }
+  }   
+  else {
+alert('wrong input')
+    
+  }
+}
